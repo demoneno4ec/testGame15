d66f0706b196dd1c0e35ded05f4fbfeeb0bf13b8
pestovmv <pestovmv@inbox.ru>
2020-10-18 15:20:56 +0300

    Изменил получение игры, так чтобы игра всегда была решаема.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -15,7 +15,8 @@ class GameController
             'chars' => ['nullable', 'max:15', new GameRule()],
         ]);
 
-        $chars = $this->getChars($validatedData['chars']);
+        $inputChars = $validatedData['chars'] ?? null;
+        $chars = $this->getChars($inputChars);
 
         $game = new Game();
         $game->chars = $chars;
@@ -24,8 +25,8 @@ class GameController
 
         return response(
             [
-                'id' => $game->id,
-                'shuffle' => str_shuffle($chars),
+                'id'      => $game->id,
+                'shuffle' => $this->shuffle($chars),
             ],
             200
         );
@@ -33,8 +34,78 @@ class GameController
 
     private function getChars(?string $chars): string
     {
-        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $response = $chars ?? '123456789abcdef';
 
-        return $chars ?? substr(str_shuffle($permitted_chars), 0, 10);
+        return $response.'0';
+    }
+
+    private function shuffle(string $chars): string
+    {
+        $shuffle = str_shuffle($chars);
+
+        if ($this->isResolved($shuffle, $chars)) {
+            return $shuffle;
+        }
+
+        return $this->swap($shuffle);
+    }
+
+    private function isResolved(string $shuffle, string $startPosition): bool
+    {
+        $arChars = str_split($shuffle);
+        $startPositions = str_split($startPosition);
+        $arStartPositions = array_flip($startPositions);
+
+        $checkedIndex = [];
+        $zeroRow = 0;
+
+        $countPars = 0;
+
+        foreach ($arChars as $position => $char) {
+            $currentIndex = $arStartPositions[$char];
+            if ($currentIndex === 15) {
+                $zeroRow = $this->getZeroRow($position);
+            }
+            $countLessIndex = $this->getLessIndex($checkedIndex, $currentIndex);
+            $countPars += $currentIndex - $countLessIndex;
+            $checkedIndex [] = $currentIndex;
+        }
+
+        return (($countPars + $zeroRow) % 2) === 0;
+    }
+
+    private function swap(string $shuffle): string
+    {
+        $indexFirstChar = 0;
+
+        if ($shuffle[0] === 0) {
+            $indexFirstChar = 1;
+        }
+
+        $indexSecondChar = $indexFirstChar + 1;
+
+        $firstChar = $shuffle[$indexFirstChar];
+        $secondChar = $shuffle[$indexSecondChar];
+
+        $shuffle[$indexFirstChar] = $secondChar;
+        $shuffle[$indexSecondChar] = $firstChar;
+
+        return $shuffle;
+    }
+
+    private function getLessIndex(array $checkedIndex, int $currentIndex): int
+    {
+        $counter = 0;
+        foreach ($checkedIndex as $v) {
+            if ($v < $currentIndex) {
+                $counter++;
+            }
+        }
+        return $counter;
+    }
+
+    private function getZeroRow(int $position)
+    {
+        return intdiv($position, 4) + ($position === 0 || $position % 4 !== 0 ? 1 : 0);
     }
 }
